1ee5bbc8982ec5750f18d3ea4afc9a95287cbcc0
Dicksen Tan <tandicksen@gmail.com>
2024-02-07 20:56:41 +0700

    done basic calculation

--- main.js
+++ main.js
@@ -4,10 +4,9 @@ const people = document.getElementById("people");
 const tipAmount = document.querySelector(".tip-amount");
 const totalPerson = document.querySelector(".total-person");
 const reset = document.querySelector(".reset-btn");
-const billError = document.querySelector('.billError')
-const peopleError = document.querySelector('.peopleError')
-const inputForm = document.querySelectorAll('.inputForm')
-
+const billError = document.querySelector(".billError");
+const peopleError = document.querySelector(".peopleError");
+const inputForm = document.querySelectorAll(".inputForm");
 
 let tipPercent = 0;
 
@@ -16,21 +15,32 @@ people.addEventListener("input", calculate);
 tips.forEach((tip) => {
   tip.addEventListener("click", () => {
     tipPercent = tip.value;
+    calculate();
   });
 });
 
-function errorCheck(input, msg, output){
-    if (input === '' || input === null){
-        output.innerText = "Can't be zero"
-    }
-    else if(!(input.value > 1 && input === people ? input.value < 1000 : input.value <= 999999)){
-        output.innerText = msg
-    }   
+function errorCheck(input, msg, output) {
+  if (input === "" || input === null) {
+    output.innerText = "Can't be zero";
+  } else if (
+    !(input.value > 1 && input === people
+      ? input.value < 1000
+      : input.value <= 999999)
+  ) {
+    output.innerText = msg;
+  }
 }
 
-function calculate(tip = "0") {
+let totalTip = 0;
+function calculate() {
+  console.log(typeof bill.value);
   console.log(tipPercent);
-  console.log(bill.value);
-  console.log(people.value);
-  console.log(tipPercent * bill.value);
+  console.log(typeof people.value);
+  if (tipPercent) {
+    //bill value is string, remember!
+    totalTip = bill.value * (tipPercent / 100);
+    console.log(totalTip);
+  }
+  totalPay = (bill.value + totalTip) / people.value;
+  console.log(totalPay);
 }
