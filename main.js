d9141f547d63bd73516e8242ec2efe1440a5015b
Dicksen Tan <tandicksen@gmail.com>
2024-02-08 19:56:29 +0700

    fixing tip function

--- main.js
+++ main.js
@@ -6,41 +6,58 @@ const totalPerson = document.querySelector(".total-person");
 const reset = document.querySelector(".reset-btn");
 const billError = document.querySelector(".billError");
 const peopleError = document.querySelector(".peopleError");
-const inputForm = document.querySelectorAll(".inputForm");
+const peopleForm = document.querySelector(".peopleForm");
+const billForm = document.querySelector(".billForm");
+const tipInput = document.querySelector(".tipInput");
 
 let tipPercent = 0;
 
 bill.addEventListener("input", calculate);
 people.addEventListener("input", calculate);
 tips.forEach((tip) => {
-  tip.addEventListener("click", () => {
-    tipPercent = tip.value;
-    calculate();
-  });
+  tip.addEventListener('click', ()=>{
+    tipCheck(tip)
+  })
 });
-
-function errorCheck(input, msg, output) {
-  if (input === "" || input === null) {
-    output.innerText = "Can't be zero";
-  } else if (
-    !(input.value > 1 && input === people
-      ? input.value < 1000
-      : input.value <= 999999)
-  ) {
-    output.innerText = msg;
-  }
+tipInput.addEventListener("input", ()=>{
+    tipCheck(tipInput)
+});
+function tipCheck(tip){
+    tips.forEach((tip) => {
+      tip.classList.remove("active");
+    });
+    tip.classList.add("active");
+    tipPercent = tip.value;
+    console.log(tipPercent);
+    calculate();
 }
+function errorCheck() {
+  noError = true;
+  function validateError(input, form, output) {
+    if (input.value === "" || input.value === null || input.value == 0) {
+      output.innerText = "Can't be zero";
+      form.style.border = "1px solid salmon";
+      noError = false;
+    } else {
+      output.innerText = "";
+      form.style.border = "none";
+    }
+  }
+  validateError(bill, billForm, billError);
+  validateError(people, peopleForm, peopleError);
+  return noError;
+}
+
+
 
 let totalTip = 0;
 function calculate() {
-  console.log(typeof bill.value);
-  console.log(tipPercent);
-  console.log(typeof people.value);
-  if (tipPercent) {
-    //bill value is string, remember!
-    totalTip = bill.value * (tipPercent / 100);
-    console.log(totalTip);
+  if (errorCheck()) {
+    
+    if (tipPercent) {
+      totalTip = bill.value * (tipPercent / 100);
+    }
+    totalPay = (parseInt(bill.value) + totalTip) / people.value;
+    console.log(totalPay);
   }
-  totalPay = (bill.value + totalTip) / people.value;
-  console.log(totalPay);
 }
